c10094b4c9c7babae1278c42b5688f255363af22
Sam Watson <watsonsam@yahoo.co.uk>
2016-09-05 22:08:59 +0100

    more markdown, unzipping script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project fulfils the requirements for the course project for week 4 of the [Coursera/Johns Hopkins University "Getting and Cleaning Data" course](https://www.coursera.org/learn/data-cleaning/home/welcome).
 
-Using the provided [data file](getdata%252Fprojectfiles%252FUCI%20HAR%20Dataset.zip) the run_analysis.R script does the following:
+Using the provided [data file](getdata%252Fprojectfiles%252FUCI%20HAR%20Dataset.zip) (the original file and more information about it can be found [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) the run_analysis.R script does the following:
 
 1. Merges the training and the test sets to create one data set.
 2. Extracts only the measurements on the mean and standard deviation for each measurement.
--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,14 @@
-##  run_analysis.R does the following
+## run_analysis.R does the following
 
-##  Merges the training and the test sets to create one data set.
-##  Extracts only the measurements on the mean and standard deviation for each measurement.
-##  Uses descriptive activity names to name the activities in the data set
-##  Appropriately labels the data set with descriptive variable names.
-##  From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Merges the training and the test sets to create one data set.
+## Extracts only the measurements on the mean and standard deviation for each measurement.
+## Uses descriptive activity names to name the activities in the data set
+## Appropriately labels the data set with descriptive variable names.
+## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+library(dplyr)
+
+## Unzip the data file
+print(list.files())
+unzip("getdata%2Fprojectfiles%2FUCI HAR Dataset.zip")
+print(list.files())
